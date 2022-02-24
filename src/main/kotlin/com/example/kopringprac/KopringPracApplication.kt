e02f625dbe850aafe91aa2b2d008859de54392e8
bmh8993 <bmh9175@gmail.com>
2022-02-25 00:12:27 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/kopringprac/KopringPracApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kopringprac
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringPracApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringPracApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kopringprac/KopringPracApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.kopringprac
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringPracApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
